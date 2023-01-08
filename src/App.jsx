1a4cb1f9362a210a39a8198678e1849d8841a5f0
Lee TB <tblyduc2412@gmail.com>
2023-01-08 21:38:48 +0700

    complete part 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import logo from '~/assets/images/logo.svg';
 import avatar from '~/assets/images/avatar.svg';
-import { BsFillPlusSquareFill, BsFillImageFill, BsFileEarmarkTextFill } from 'react-icons/bs';
+import {
+  BsFillPlusSquareFill,
+  BsFillImageFill,
+  BsFileEarmarkTextFill,
+  BsLaptopFill,
+  BsFillTabletFill,
+  BsFillPhoneFill
+} from 'react-icons/bs';
 import { RiLayoutMasonryFill } from 'react-icons/ri';
 import { IoIosColorPalette, IoMdSettings, IoIosAlert } from 'react-icons/io';
+import { GoChevronLeft } from 'react-icons/go';
+import { BiChevronDown } from 'react-icons/bi';
+import { AiFillEye } from 'react-icons/ai';
+import { SiWebpack } from 'react-icons/si';
 import Tooltip from './components/Tooltip';
 
 function App() {
   return (
     <div className="flex bg-gray-100 font-sans text-gray-900">
-      <aside className="w-18 h-screen border-r border-gray-200 bg-white flex flex-col items-center">
-        <div className="h-18 w-full flex items-center justify-center border-b border-gray-200">
+      <aside className="flex h-screen w-18 flex-col items-center border-r border-gray-200 bg-white">
+        <div className="flex h-18 w-full items-center justify-center border-b border-gray-200">
           <a href="#" className="inline w-2/5">
             <img src={logo} alt="Logo" className="block" />
           </a>
@@ -17,7 +28,7 @@ function App() {
         <nav className="flex flex-1 flex-col gap-y-4 pt-10">
           <a
             href="#"
-            className="group relative rounded-xl p-3 text-gray-400 hover:text-blue-600 hover:bg-gray-100 mx-auto">
+            className="group relative mx-auto rounded-xl p-3 text-gray-400 hover:bg-gray-100 hover:text-blue-600">
             <BsFillPlusSquareFill className="h-6 w-6" />
             <Tooltip>
               Add elements
@@ -26,7 +37,7 @@ function App() {
           </a>
           <a
             href="#"
-            className="group relative rounded-xl p-3 text-gray-400 hover:text-blue-600 hover:bg-gray-100 mx-auto">
+            className="group relative mx-auto rounded-xl p-3 text-gray-400 hover:bg-gray-100 hover:text-blue-600">
             <RiLayoutMasonryFill className="h-6 w-6" />
             <Tooltip>
               Layout
@@ -35,7 +46,7 @@ function App() {
           </a>
           <a
             href="#"
-            className="group relative rounded-xl p-3 text-gray-400 hover:text-blue-600 hover:bg-gray-100 mx-auto">
+            className="group relative mx-auto rounded-xl p-3 text-gray-400 hover:bg-gray-100 hover:text-blue-600">
             <IoIosColorPalette className="h-6 w-6" />
             <Tooltip>
               Theme
@@ -44,7 +55,7 @@ function App() {
           </a>
           <a
             href="#"
-            className="group relative rounded-xl p-3 text-gray-400 hover:text-blue-600 hover:bg-gray-100 mx-auto">
+            className="group relative mx-auto rounded-xl p-3 text-gray-400 hover:bg-gray-100 hover:text-blue-600">
             <BsFileEarmarkTextFill className="h-6 w-6" />
             <Tooltip>
               Assets
@@ -53,7 +64,7 @@ function App() {
           </a>
           <a
             href="#"
-            className="group relative rounded-xl p-3 text-gray-400 hover:text-blue-600 hover:bg-gray-100 mx-auto">
+            className="group relative mx-auto rounded-xl p-3 text-gray-400 hover:bg-gray-100 hover:text-blue-600">
             <BsFillImageFill className="h-6 w-6" />
             <Tooltip>
               Images
@@ -65,21 +76,69 @@ function App() {
         <div className="flex flex-col items-center gap-y-4 py-10">
           <a
             href="#"
-            className="group relative rounded-xl p-3 text-gray-400 hover:text-blue-600 hover:bg-gray-100 mx-auto">
+            className="group relative mx-auto rounded-xl p-3 text-gray-400 hover:bg-gray-100 hover:text-blue-600">
             <IoIosAlert className="h-6 w-6" />
             <Tooltip>Alert</Tooltip>
           </a>
           <a
             href="#"
-            className="group relative rounded-xl p-3 text-gray-400 hover:text-blue-600 hover:bg-gray-100 mx-auto">
+            className="group relative mx-auto rounded-xl p-3 text-gray-400 hover:bg-gray-100 hover:text-blue-600">
             <IoMdSettings className="h-6 w-6" />
             <Tooltip>Settings</Tooltip>
           </a>
-          <button className="pt-2 overflow-hidden rounded-full">
+          <button className="overflow-hidden rounded-full pt-2">
             <img className="h-10 w-10" src={avatar} alt="" />
           </button>
         </div>
       </aside>
+      <div className="flex h-screen flex-1 flex-col">
+        <header className="flex h-18 items-center justify-center gap-x-4 border-b border-gray-200 bg-white px-6">
+          <button className="group flex items-center justify-center rounded-xl bg-gray-100 p-3 focus:ring focus:ring-gray-200">
+            <GoChevronLeft className="h-6 w-6 text-gray-400 group-hover:text-blue-600" />
+          </button>
+          <button className="flex flex-col justify-center rounded-xl bg-gray-100 px-3 py-2 focus:ring focus:ring-gray-200">
+            <div className="flex items-baseline">
+              <span className="text-sm font-semibold text-gray-900">Page: Homepage - Dipa</span>
+              <BiChevronDown className="h-4 w-4 text-gray-400" />
+            </div>
+            <div className="text-xs text-gray-400">https://www.dipainhouse.com/</div>
+          </button>
+          <button className="group flex items-center justify-center gap-1 rounded-xl bg-gray-100 p-3 focus:ring focus:ring-gray-200">
+            <AiFillEye className="h-6 w-6 text-gray-400 group-hover:text-blue-600" />
+            <span className="font-semibold text-gray-900 group-hover:text-blue-600">Preview</span>
+          </button>
+
+          <div className="h-full w-px bg-gray-200" />
+
+          <div className="flex items-center gap-x-2">
+            <button className="group flex items-center justify-center rounded-xl p-3 hover:bg-gray-100 focus:ring focus:ring-gray-200">
+              <BsLaptopFill className="h-6 w-6 text-gray-400 group-hover:text-blue-600" />
+            </button>
+            <button className="group flex items-center justify-center rounded-xl p-3 hover:bg-gray-100 focus:ring focus:ring-gray-200">
+              <BsFillTabletFill className="h-6 w-6 text-gray-400 group-hover:text-blue-600" />
+            </button>
+            <button className="group flex items-center justify-center rounded-xl p-3 hover:bg-gray-100 focus:ring focus:ring-gray-200">
+              <BsFillPhoneFill className="h-6 w-6 text-gray-400 group-hover:text-blue-600" />
+            </button>
+            <button className="group flex items-center justify-center rounded-xl p-3 hover:bg-gray-100 focus:ring focus:ring-gray-200">
+              <BsFillPhoneFill className="h-6 w-6 rotate-90 text-gray-400 group-hover:text-blue-600" />
+            </button>
+          </div>
+
+          <div className="h-full w-px bg-gray-200" />
+
+          <button className="flex flex-col justify-center rounded-xl bg-gray-100 p-3 focus:ring focus:ring-gray-200">
+            <div className="flex items-baseline">
+              <span className="text-sm font-bold text-gray-900">960 PX/100%</span>
+              <BiChevronDown className="h-4 w-4 text-gray-400" />
+            </div>
+          </button>
+          <button className="group flex items-center justify-center rounded-xl bg-gray-100 p-3 focus:ring focus:ring-gray-200">
+            <SiWebpack className="h-6 w-6 text-gray-400 group-hover:text-blue-600" />
+          </button>
+        </header>
+      </div>
+      <aside className="h-screen w-[300px]"></aside>
     </div>
   );
 }
